13db36612996651b07446e0734e3951bd5f80632
Farzad Afshari <far.afshari@gmail.com>
2014-04-26 00:04:09 -0400

    Created CellScript with pseudo code for dividing

---
+++ Assets/Cell/CellScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Gamelogic.Grids;
+
+public class CellScript : MonoBehaviour {
+
+	public float _divideDelayInSeconds = 2;
+	float _divideChance = 5;
+	float mutateChance;
+	
+	FlatHexGrid<CellScript> grid;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		_divideChance = 1 / (_divideDelayInSeconds * (1 / Time.fixedDeltaTime));
+
+		DivideCell ();
+
+	}
+
+	void DivideCell () {
+		float rng = Random.Range (0.0f,1.0f);
+
+		if (_divideChance > rng) {
+			Debug.Log ("Success");
+			//List<FlatHexPoint> neighbours = grid.GetNeighbors(point);
+			//List<FlatHexPoint> free = neighbours.Where ((point) => grid[point] == null);
+
+			//pointToSpawn = free[Random.Range(0,free.Count)];
+			//grid.create(pointToSpawn);
+		}
+	}
+	bool IsEmpty (FlatHexPoint point) {
+		return grid[point] == null;
+	}
+
+}
