1ff6887f6d6f084b240515eb3061d89b840374fa
Patrick McKenna <patrick.a.mckenna@gmail.com>
2014-04-26 22:58:00 -0400

    add and enable the laser surgery

---
+++ Assets/Surgury/LaserTool.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Gamelogic.Grids;
+
+[RequireComponent (typeof(LineRenderer))]
+public class LaserTool : Treatment {
+
+	public bool startPositionSet;
+
+	public Vector3 startPoint;
+	public Vector3 endPoint;
+
+	protected LineRenderer lineRenderer;
+	
+	void OnEnable () {
+		startPositionSet = false;
+		lineRenderer = gameObject.GetComponent<LineRenderer>();
+		lineRenderer.enabled = false;
+	}
+
+	void Update () {
+		if (!startPositionSet) {
+			// user hasn't set the start point yet
+			if (Input.GetMouseButtonDown(0)) {
+				startPoint = ExampleUtils.ScreenToWorld(area.root, Input.mousePosition);
+				startPoint = new Vector3(startPoint.x, startPoint.y, -1);
+				lineRenderer.SetPosition(0, startPoint);
+				lineRenderer.enabled = true;
+				startPositionSet = true;
+			}
+		} else {
+			endPoint = ExampleUtils.ScreenToWorld(area.root, Input.mousePosition);
+			endPoint = new Vector3(endPoint.x, endPoint.y, -1);
+
+			lineRenderer.SetPosition(1, endPoint);
+
+			if (Input.GetMouseButtonDown(0)) {
+
+				float dist = Vector3.Distance(startPoint, endPoint);
+				for (int i = 0; i < dist; i++) {
+					float t = dist / (float) i;
+					Vector3 test = Vector3.Lerp(startPoint, endPoint, t);
+					FlatHexPoint testPos = area.map[test];
+					if (area.grid[testPos] != null) {
+						Destroy(area.grid[testPos]);
+					}
+				}
+
+				Use();
+			}
+		}
+	}
+}
